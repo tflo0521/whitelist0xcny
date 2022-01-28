6b8ce56388a30f4ae550554d6a92a7ebab27c875
tflo0521 <97916946+tflo0521@users.noreply.github.com>
2022-01-28 15:10:57 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -169,7 +169,7 @@ export default function Mint() {
     
                 </div>
                 {saleStarted ? 
-                <button onClick={() => mintbaby(how_many_babys)} className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">MINT {how_many_babys} babys for {(babyPrice * how_many_babys) / (10 ** 18)} ETH + GAS</button>        
+                <button onClick={() => mintbaby(how_many_babys)} className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">MINT BABYIES</button>        
                   : <button className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">SALE IS NOT ACTIVE OR NO WALLET IS CONNECTED</button>        
             
               }
