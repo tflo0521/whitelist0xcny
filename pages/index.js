c2d4c84480258a2cea341a91973be0f44fbee0b8
tflo0521 <97916946+tflo0521@users.noreply.github.com>
2022-02-06 20:40:35 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -169,7 +169,7 @@ export default function Mint() {
     
                 </div>
                 {saleStarted ? 
-                <button onClick={() => mintbaby(how_many_babys)} className="mt-4 PalatinoLinotype text-3xl border-6 bg-blau  text-white hover:text-black p-2 ">MINT</button>        
+                <button onClick={() => mintbaby(how_many_babys)} className="mt-4 PalatinoLinotype text-3xl border-6 bg-white text-black hover:text-white p-2 ">MINT</button>        
                   : <button className="mt-4 PalatinoLinotype text-3xl border-6 bg-blau  text-white hover:text-black p-2 ">SALE IS NOT ACTIVE OR NO WALLET IS CONNECTED</button>        
             
               }
