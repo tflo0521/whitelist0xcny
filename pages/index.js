5f8177e55e2f47479e94619f4d57ec84197f87ee
tflo0521 <97916946+tflo0521@users.noreply.github.com>
2022-02-07 14:56:53 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,101 @@ import { useState, useEffect } from 'react';
 
 import {ADDRESS, ABI} from "../config.js"
 
+export default function privateMint() {
+
+  // FOR WALLET
+  const [signedIn, setSignedIn] = useState(false)
+
+  const [walletAddress, setWalletAddress] = useState(null)
+
+  // FOR MINTING
+  const [how_many_xCny, set_how_many_xCny] = useState(1)
+
+  const [xCnyContract, setxCnyContract] = useState(null)
+
+  // INFO FROM SMART Contract
+
+  const [totalSupply, setTotalSupply] = useState(0)
+
+  const [saleStarted, setSaleStarted] = useState(false)
+
+  const [privatePrice, setprivatePrice] = useState(0)
+
+  useEffect( async() => { 
+
+    signIn()
+
+  }, [])
+
+  async function signIn() {
+    if (typeof window.web3 !== 'undefined') {
+      // Use existing gateway
+      window.web3 = new Web3(window.ethereum);
+
+    } else {
+      alert("No Ethereum interface injected into browser. Read-only access");
+    }
+
+    window.ethereum.enable()
+      .then(function (accounts) {
+        window.web3.eth.net.getNetworkType()
+        // checks if connected network is mainnet (change this to rinkeby if you wanna test on testnet)
+        .then((network) => {console.log(network);if(network != "main"){alert("You are on " + network+ " network. Change network to mainnet or you won't be able to do anything here")} });
+        let wallet = accounts[0]
+        setWalletAddress(wallet)
+        setSignedIn(true)
+        callContractData(wallet)
+
+  })
+  .catch(function (error) {
+  // Handle error. Likely the user rejected the login
+  console.error(error)
+  })
+  }
+
+//
+
+  async function signOut() {
+    setSignedIn(false)
+  }
+
+  async function callContractData(wallet) {
+    // let balance = await web3.eth.getBalance(wallet);
+    // setWalletBalance(balance)
+    const xCnyContract = new window.web3.eth.Contract(ABI, ADDRESS)
+    setxCnyContract(xCnyContract)
+
+    const salebool = await xCnyContract.methods.saleIsActive().call() 
+    // console.log("saleisActive" , salebool)
+    setSaleStarted(salebool)
+
+    const totalSupply = await xCnyContract.methods.totalSupply().call() 
+    setTotalSupply(totalSupply)
+
+  }
+
+  async function mintxCny(how_many_xCny) {
+    if (xCnyContract) {
+ 
+      const price = Number(privatePrice)  * how_many_xCny 
+
+      const gasAmount = await xCnyContract.methods.publicMintxCny(how_many_xCny).estimateGas({from: walletAddress, value: price})
+      console.log("estimated gas",gasAmount)
+
+      console.log({from: walletAddress, value: price})
+
+      xCnyContract.methods
+            .publicMintxCny(how_many_xCny)
+            .send({from: walletAddress, value: price, gas: String(gasAmount)})
+            .on('transactionHash', function(hash){
+              console.log("transactionHash", hash)
+            })
+
+    } else {
+        console.log("Wallet not connected")
+    }
+
+  };
 
 
 
